81ddd486f7c70f878140d94a9308e98506057704
paullaster <paullasterokoth98@gmail.com>
2022-09-05 16:36:34 +0300

    Solving Daily events avarage/channel using WITH: CTE

--- daily_web_events_per_channel.sql
+++ daily_web_events_per_channel.sql
@@ -12,3 +12,21 @@ FROM (
     GROUP BY dy, channel
 )eve_avg
 GROUP BY channel
+
+
+/************************************************************************************************/
+/*PROVIDING SAME SOLUTION USING WITH: CTE, TO ENHANCE FAST COMPUTATION*/
+
+WITH events  AS (
+    SELECT 
+        DATE_TRUNC('day', occurred_at) dy,
+        channel,
+        COUNT(*) number_of_events
+    FROM web_events
+    GROUP BY dy, channel
+)
+SELECT 
+    channel,
+    AVG(number_of_events) "Average number of events for each channel per day"
+FROM events
+GROUP BY channel
